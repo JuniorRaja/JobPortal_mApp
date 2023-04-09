f001db19a9b7f056030ef273526a260f0f3a1b51
Prasanna Rajendran <imprasannarajendran@gmail.com>
2023-04-09 23:51:56 +0530

    Project Sttucture & Assets Added

--- app/index.js
+++ app/index.js
@@ -1,6 +1,17 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, imgages, SIZES } from "../constants";
+import {
+  NearbyJobs,
+  PopularJobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+
   return (
     <View>
       <Text> Home </Text>
